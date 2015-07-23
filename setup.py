602f2a89aac9ded0631b178f394990ac04bdb237
Niklas Baumstark <niklas.baumstark@gmail.com>
2015-07-23 20:25:35 +0200

    bump version because we forget a file in the MANIFEST

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ class build_server(_build):
     shutil.copy('src/webkit_server', self.build_platlib)
 
 setup(name='webkit-server',
-      version='0.9',
+      version='0.9.1',
       description='a Webkit-based, headless browser instance',
       author='Niklas Baumstark',
       author_email='niklas.baumstark@gmail.com',
